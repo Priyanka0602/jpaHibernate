a7919c88e19801f45b583a7c5129eca50787adc1
Priyanka <priyanka.agrawal@kroger.com>
2021-05-06 08:53:11 +0530

    adds Enumerated

--- src/main/java/dao/User.java
+++ src/main/java/dao/User.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
 
 @Entity
 @Table(name = "users")
@@ -13,38 +15,16 @@ public class User {
     private long id;
 
    private String name;
-   private String role;
 
-    public User(String name, String role) {
+   @Enumerated(EnumType.STRING)
+   private RoleEnum role;
+
+    public User(String name, RoleEnum role) {
         this.name = name;
         this.role = role;
     }
 
     public User() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
     }
 
     @Override
